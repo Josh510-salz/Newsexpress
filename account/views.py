ad19717cf057854f846e548a38af0654f9f35a8b
Salami Joshua <salamijijoshua510@gmail.com>
2024-03-19 14:15:13 +0100

    initial-commit

---
+++ account/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.GET)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Account Authenticated Succesfully')
+                else:
+                    return HttpResponse('Disabled Account')
+            else:
+                return HttpResponse('Invaalid login credentials')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+# Create your views here.
